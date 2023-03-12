88ce72614b0b5cdfeb1fb121e24c017326194e09
Sebastian Blazejewski <blazejewski@post.pl>
2023-03-12 03:44:44 +0100

    add new functions add styling and move menu box to result input

--- my-wallet/src/app/menu-box/menu-box.component.ts
+++ my-wallet/src/app/menu-box/menu-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu-box',
@@ -6,60 +6,5 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./menu-box.component.css']
 })
 export class MenuBoxComponent {
-  optionsNeeds: string[] = ['Mieszkanie', 'Prąd', 'Internet', 'Telefon', 'Paliwo'];
-  optionsWants: string[] = ['Ciuchy', 'Restauracja', 'Kino', 'Impreza', 'Narkotyki'];
-  optionsSavings: string[] = ['Raty', 'Krypto', 'Skarbonka', 'Na wakacje', 'Emerytura']
-  selectedOption = ' ';
-  newOption = '';
-  @Input() result20 = 0;
-  @Input() resultNumber = 0;
-  @Input() number = 0;
-  @Output() resultEvent = new EventEmitter<{ resultNumber: number, result50: number, result30: number, result20: number }>();
 
-
-
-  onSelectChange() {
-    if (this.selectedOption === 'add-new-option') {
-      this.newOption = '';
-    }
-  }
-
-  addNewOptionNeeds() {
-    if (this.newOption && !this.optionsNeeds.includes(this.newOption)) {
-      this.optionsNeeds.push(this.newOption);
-      this.selectedOption = this.newOption;
-    }
-  }
-  addNewOptionWants() {
-    if (this.newOption && !this.optionsWants.includes(this.newOption)) {
-      this.optionsWants.push(this.newOption);
-      this.selectedOption = this.newOption;
-    }
-  }
-  addNewOptionSavings() {
-    if (this.newOption && !this.optionsSavings.includes(this.newOption)) {
-      this.optionsSavings.push(this.newOption);
-      this.selectedOption = this.newOption;
-    }
-  }
-  substractSavings() {
-          const inputElement = document.getElementById('numberInput') as HTMLInputElement;
-      if (inputElement != null) {
-        const subtractedValue = Number(inputElement.value);
-        const currentResults = {
-          resultNumber: this.number,
-          result50: this.number * 0.5,
-          result30: this.number * 0.3,
-          result20: this.number * 0.2
-        };
-        const updatedResult = {
-          resultNumber: this.number - subtractedValue,
-          result50: (this.number - subtractedValue) * 0.5,
-          result30: (this.number - subtractedValue) * 0.3,
-          result20: (this.number - subtractedValue) * 0.2
-        };
-        this.number -= subtractedValue;
-        this.resultEvent.emit(updatedResult);
-      }
-    }
-  }
+}
--- my-wallet/src/app/result-input/result-input.component.ts
+++ my-wallet/src/app/result-input/result-input.component.ts
@@ -11,5 +11,72 @@ export class ResultInputComponent {
   @Input() result50 = 0;
   @Input() result30 = 0;
   @Input() result20 = 0;
-}
+  optionsNeeds: string[] = ['Mieszkanie', 'Prąd', 'Internet', 'Telefon', 'Paliwo'];
+  optionsWants: string[] = ['Ciuchy', 'Restauracja', 'Kino', 'Impreza', 'Narkotyki'];
+  optionsSavings: string[] = ['Raty', 'Krypto', 'Skarbonka', 'Na wakacje', 'Emerytura']
+  selectedOptionNeeds = ' ';
+  selectedOptionWants = ' ';
+  selectedOptionSavings = ' ';
+  newOptionNeeds = '';
+  newOptionWants = '';
+  newOptionSavings = '';
+  allResults = 0;
+  substractionSavings!: number;
+  substractionNeeds!: number;
+  substractionWants!: number;
 
+  
+  
+  subtractNeeds() {
+    if (this.result50) {
+      this.result50 -= this.substractionNeeds;
+      this.allResults = this.result20 + this.result30 + this.result50
+
+    }
+  }
+  subtractWants() {
+    if (this.result30) {
+      this.result30 -= this.substractionWants;
+      this.allResults = this.result20 + this.result30 + this.result50
+
+    }
+  }
+ 
+  subtractSavings() {
+    if (this.result20) {
+      this.result20 -= this.substractionSavings;
+      this.allResults = this.result20 + this.result30 + this.result50;
+      const paragraph = document.getElementById('total-balance');
+      if (paragraph) {
+        if (this.allResults < 0) {
+          paragraph.style.color = 'red';
+        } else {
+          paragraph.style.color = '#06D6A0';
+        }
+      }
+    }
+  }
+  onSelectChange() {
+    if (this.selectedOptionNeeds || this.selectedOptionWants || this.selectedOptionSavings === 'add-new-option') {
+      this.newOptionNeeds || this.newOptionWants || this.newOptionSavings;
+    }
+  }
+
+  addNewOptionNeeds() {
+    if (this.newOptionNeeds && !this.optionsNeeds.includes(this.newOptionNeeds)) {
+      this.optionsNeeds.push(this.newOptionNeeds);
+      this.selectedOptionNeeds = this.newOptionNeeds;
+    }
+  }
+  addNewOptionWants() {
+    if (this.newOptionWants && !this.optionsWants.includes(this.newOptionWants)) {
+      this.optionsWants.push(this.newOptionWants);
+      this.selectedOptionWants = this.newOptionWants;
+    }
+  }
+  addNewOptionSavings() {
+    if (this.newOptionSavings && !this.optionsSavings.includes(this.newOptionSavings)) {
+      this.optionsSavings.push(this.newOptionSavings);
+      this.selectedOptionSavings = this.newOptionSavings;
+    }
+  }}
