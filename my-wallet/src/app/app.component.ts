d47657dc4f509ea7fd7c65fc8a7ff43b96ee5e7b
Sebastian Błażejewski <74263390+smootny@users.noreply.github.com>
2023-03-12 14:52:27 +0100

    Calculate function (#1)
    * add function for calculate
    
    * add new functions add styling and move menu box to result input

--- my-wallet/src/app/app.component.ts
+++ my-wallet/src/app/app.component.ts
@@ -9,38 +9,9 @@ import { Component} from '@angular/core';
 })
 
 export class AppComponent {
-  currentBlock: { size: string, color: string } | null = null;
-  showBlock = false;
   resultNumber = 0;
   result50 = 0;
   result30 = 0;
-  result20 = 0;
-  
-  // Funkcja generująca losową wielkość bloku w pikselach
-  generateBlockSize(): string {
-    const size = Math.floor(Math.random() * 100) + 50;
-    return size + 'px';
-  }
-  
-  // Funkcja generująca losowy kolor w formacie HEX
-  generateBlockColor(): string {
-    const color = Math.floor(Math.random() * 16777215).toString(16);
-    return '#' + color;
-  }
-  
-  // Funkcja generująca blok w losowym rozmiarze i kolorze i ustawiająca go jako aktualny blok
-  generateBlock(): void {
-    const size = this.generateBlockSize();
-    const color = this.generateBlockColor();
-    this.currentBlock = { size, color };
-    this.showBlock ? this.showBlock = false : this.showBlock = true;
-  }
-  
-  // Funkcja usuwająca aktualny blok i ustawiająca zmienną showBlock na wartość false
-  hideBlock(): void {
-    this.currentBlock = null;
-    this.showBlock = false;
-  }
-  
-  
-}
+  result20 = 0; 
+  result!: number;
+} 
--- my-wallet/src/app/generate-button/generate-button.component.ts
+++ my-wallet/src/app/generate-button/generate-button.component.ts
@@ -10,13 +10,16 @@ import { Component, Output, EventEmitter } from '@angular/core';
 
 export class GenerateButtonComponent {
   @Output() resultEvent = new EventEmitter<{ resultNumber: number, result50: number, result30: number, result20: number }>();
+  @Output() resultSavings = new EventEmitter<{resultat: number}>();
   number = 0;
+  @Output() result = new EventEmitter<number>();
+  inputValue = 0;
 
   calculate() {
-    const result50 = this.number * 0.5;
-    const result30 = this.number * 0.3;
-    const result20 = this.number * 0.2;
-    this.resultEvent.emit({ resultNumber: this.number, result50, result30, result20 });
+    const result50 = this.inputValue * 0.5;
+    const result30 = this.inputValue * 0.3;
+    const result20 = this.inputValue * 0.2;
+    this.resultEvent.emit({ resultNumber: this.inputValue, result50, result30, result20 });
   }
 }
 
